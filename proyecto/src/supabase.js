4e16a0589c3c1b10dddb09bb7e2f1376dc9961b5
AngieNeiza <angie.neizac@uniagustiniana.edu.co>
2025-11-25 17:29:37 +0000

    actualizacion

--- proyecto/src/supabase.js
+++ proyecto/src/supabase.js
@@ -1,5 +1,5 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://gqebtwnadrhoqtyfjsgv.supabase.co/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZWJ0d25hZHJob3F0eWZqc2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjIxNTMsImV4cCI6MjA3OTU5ODE1M30.y_-vyyWe1k4RmYsFumkuEE-TO-A2n5OKBHncUg6KlkM';
+const supabaseUrl = 'https://nqgthxmkrwvjtcjrjafp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xZ3RoeG1rcnd2anRjanJqYWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNTgxODcsImV4cCI6MjA3OTYzNDE4N30.lqRruv0xIeiBXJCFc2tEFpSuYRrNwiHDsu5N6fb7vfI';
 export const supabase = createClient(supabaseUrl, supabaseKey);
